89d8f51d2eb8db966555011283fcd2afef8856f2
WeebSlime <77358734+WeebSlime@users.noreply.github.com>
2024-01-03 12:13:41 +0530

    Update script.js

--- Particle rain/js/script.js
+++ Particle rain/js/script.js
@@ -5,7 +5,7 @@ let image;
 let imgData;
 
 (async function() {
-  image = await loadImage("../zoro.jpg");
+  image = await loadImage("./zoro.jpg");
   imgData = getImageData(image).data;
 
   /* draw pixelated image */
